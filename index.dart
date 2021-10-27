e6956c246f018ede038cbb3157e9eeabd78c277e
brangkak3 <fauzanwadi7@gmail.com>
2021-10-27 11:40:54 +0800

    segitiga pohon

---
+++ index.dart
@@ -0,0 +1,13 @@
+
+
+import 'dart:io';
+
+var n = 5;
+void main() {
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < i; j++){
+        stdout.write('\u{1f333}');
+      }
+      print('*');
+    }
+}
